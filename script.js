c6f5c0bbc6d3343f411e242b801fc7df0b1ba9d1
tgl1503 <115742248+tgl1503@users.noreply.github.com>
2025-10-04 14:04:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Rolagem suave
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const destino = document.querySelector(link.getAttribute('href'));
+    destino.scrollIntoView({ behavior: 'smooth' });
+  });
+});
